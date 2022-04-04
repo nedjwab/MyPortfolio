fc5521d7e7ab04f6eb5c56f7f87adcf51a6a1cf9
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-04-05 00:15:31 +0100

    Fix linter errors

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const body= document.querySelector("body")
-const headline=document.querySelector(".headlineSection");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const body = document.querySelector('body');
+const headline = document.querySelector('.headlineSection');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  body.style.overflow="hidden";
-  headline.classList.toggle("active");
- 
-  /*body.style.backgroundColor="rgba(0, 0, 0, 0.3)";*/
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  body.style.overflow = 'hidden';
+  headline.classList.toggle('active');
 });
-document.querySelectorAll(".nav-item").forEach(e =>e.addEventListener("click",()=>  {
-hamburger.classList.remove("active");
-navMenu.classList.remove("active");
-body.style.overflow="scroll";
-headline.classList.remove("active");
-  
+document.querySelectorAll('.nav-item').forEach((e) => e.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  body.style.overflow = 'scroll';
+  headline.classList.remove('active');
 }));
