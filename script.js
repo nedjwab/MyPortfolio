a0f509637b9b6fdc93217411fb96f91b4250ed81
Muhmmd-Usama <mummdusama@gmail.com>
2022-04-08 01:35:23 +0200

    Complet local storage function

--- script.js
+++ script.js
@@ -199,6 +199,23 @@ form.addEventListener('submit', (event) => {
 
 // ######## Local storage ########
 
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      (e instanceof DOMException && e.code === 22)
+      || e.code === 1014 || e.name === 'QuotaExceededError'
+      || (e.name === 'NS_ERROR_DOM_QUOTA_REACHED' && storage && storage.length !== 0)
+    );
+  }
+}
+
 if (storageAvailable('localStorage')) {
   const inputs = [form.name, form.email, form.message];
   inputs.forEach((input) => {
