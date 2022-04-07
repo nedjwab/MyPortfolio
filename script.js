14be68faba0cc27e28ae18e753b747d683cd87a3
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-04-07 12:24:04 +0100

    Fix linter errors

--- script.js
+++ script.js
@@ -191,7 +191,9 @@ form.addEventListener('submit', (event) => {
   if ((emailValue.toLowerCase() !== emailValue)) {
     event.preventDefault();
     text.classList.add('active');
-    text.innerHTML = 'Please Enter an email adress in Lowercase';} 
+    text.innerHTML = 'Please Enter an email adress in Lowercase';
+  } 
   else {
-    form.submit();}
+    form.submit();
+  }
 });
